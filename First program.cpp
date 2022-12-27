0f735d6c77802db10f1461d6079b2ecf9f5ec3e7
Urooj-Islam <121480546+Urooj-Islam@users.noreply.github.com>
2022-12-27 12:32:47 +0500

    Add files via upload

---
+++ First program.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"HELLO WORLD\n";
+	cout<<"I am UROOJ ISLAM KHAN\n";
+	cout<<"Student of software engineering\n";
+	cout<<"Learning C++ Programming language";
+	return 0;
+}
