8833c9c48996e70d70ea7d902a57822e3faca255
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2022-12-29 13:06:04 +0500

    Add files via upload

---
+++ Find size of character.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"The size of "<<sizeof(char)<<" byte\n";
+	cout<<"The size of "<<sizeof(int)<<" bytes\n";
+	cout<<"The size of "<<sizeof(float)<<" bytes\n";	
+	cout<<"The size of "<<sizeof(double)<<" bytes\n";
+}
